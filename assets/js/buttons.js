58710208fb9bceb912a3af541463b9754ed9fce6
Vovan533 <vovan533v@gmail.com>
2021-11-28 00:01:09 +0300

    update

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -30,18 +30,18 @@ var main_html = `
     <p>Много текста о романе...</p>
 </section>
 
-<section id='actions'>
-    <h2> События</h2>
-    <p>Не очень много текста о событиях...</p>
-    <p>Не очень много текста о событиях...</p>
-    <a id="actions" href="actions.html">Перейти на страницу с событиями</a>
+<section id='actions_section'>
+      <h2> События</h2>
+      <p>Не очень много текста о событиях...</p>
+      <p>Не очень много текста о событиях...</p>
+      <a id="actions_href" class="section_button">Перейти на страницу с событиями</a>
 </section>
 
-<section id='generals'>
-    <h2> Генералы</h2>
-    <p>Не очень много текста о генералах...</p>
-    <p>Не очень много текста о генералах...</p>
-    <a id="generals" href="generals.html">Перейти на страницу с генералами</a>
+<section id='generals_section'>
+      <h2> Генералы</h2>
+      <p>Не очень много текста о генералах...</p>
+      <p>Не очень много текста о генералах...</p>
+      <a id="generals_href" class="section_button">Перейти на страницу с генералами</a>
 </section>
 `
 var info_html = `
@@ -58,22 +58,35 @@ var info_html = `
     <p>Не очень много текста об источниках...</p>
 </section>
 `
-var actions_html = `
-<section id='generals'>
-    <h2> Генералы</h2>
-    <p>Не очень много текста о генералах...</p>
-    <p>Не очень много текста о генералах...</p>
-    <a id="generals" href="generals.html">Перейти на страницу с генералами</a>
-</section>
-`
 var generals_html = `
-<section id='actions'>
-    <h2> События</h2>
-    <p>Не очень много текста о событиях...</p>
-    <p>Не очень много текста о событиях...</p>
-    <a id="actions" href="actions.html">Перейти на страницу с событиями</a>
+<section>
+    <h2> Генерал 1</h2>
+    <p>Не очень много текста о генерале 1...</p>
+    <p>Не очень много текста о генерале 1...</p>
+    <p>Не очень много текста о генерале 1...</p>
+</section>
+<section>
+    <h2> Генерал 2</h2>
+    <p>Не очень много текста о генерале 2...</p>
+    <p>Не очень много текста о генерале 2...</p>
+    <p>Не очень много текста о генерале 2...</p>
 </section>
 `
+var actions_html = `
+<section>
+    <h2> Событие 1</h2>
+    <p>Не очень много текста о собитие 1...</p>
+    <p>Не очень много текста о собитие 1...</p>
+    <p>Не очень много текста о собитие 1...</p>
+</section>
+<section>
+    <h2> Событие 2</h2>
+    <p>Не очень много текста о собитие 2...</p>
+    <p>Не очень много текста о собитие 2...</p>
+    <p>Не очень много текста о собитие 2...</p>
+</section>
+`
+
 
 function load_info(){
     document.getElementById("wr").innerHTML = info_html;
@@ -81,16 +94,6 @@ function load_info(){
     button.classList.add("header_button")
     button.classList.remove("header_button_active")
     f_button = document.getElementById("info")
-    f_button.classList.add("header_button_active")
-    f_button.classList.remove("header_button")
-};
-
-function load_main(){
-    document.getElementById("wr").innerHTML = main_html;
-    button = document.getElementsByClassName("header_button_active")[0]
-    button.classList.add("header_button")
-    button.classList.remove("header_button_active")
-    f_button = document.getElementById("home")
     f_button.classList.add("header_button_active")
     f_button.classList.remove("header_button")
 };
@@ -105,8 +108,34 @@ function load_actions(){
     f_button.classList.remove("header_button")
 };
 
-document.getElementById("info").onclick = load_info();
+function load_generals(){
+    document.getElementById("wr").innerHTML = generals_html;
+    button = document.getElementsByClassName("header_button_active")[0]
+    button.classList.add("header_button")
+    button.classList.remove("header_button_active")
+    f_button = document.getElementById("generals")
+    f_button.classList.add("header_button_active")
+    f_button.classList.remove("header_button")
+};
 
-document.getElementById("home").onclick = load_main();
+function load_main(){
+    document.getElementById("wr").innerHTML = main_html;
+    button = document.getElementsByClassName("header_button_active")[0]
+    button.classList.add("header_button")
+    button.classList.remove("header_button_active")
+    f_button = document.getElementById("home")
+    f_button.classList.add("header_button_active")
+    f_button.classList.remove("header_button")
+};
 
-document.getElementById("actions_href").onclick = load_actions();
+document.getElementById("info").onclick = load_info;
+
+document.getElementById("home").onclick = load_main;
+
+document.getElementById("actions").onclick = load_actions;
+
+document.getElementById("generals").onclick = load_generals;
+
+document.getElementById("actions_href").onclick = load_actions;
+
+document.getElementById("generals_href").onclick = load_generals;
